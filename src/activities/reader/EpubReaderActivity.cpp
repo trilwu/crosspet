505e211471be336b2f2b5352bf76f7c938cc1976
Jorge Arroyo <trozo@makasoft.net>
2026-03-09 22:29:07 +0100

    Added logic to reader

--- src/activities/reader/EpubReaderActivity.cpp
+++ src/activities/reader/EpubReaderActivity.cpp
@@ -133,8 +133,13 @@ void EpubReaderActivity::loop() {
     }
   }
 
+  if (mappedInput.wasReleased(MappedInputManager::Button::Power) &&
+      SETTINGS.shortPwrBtn == CrossPointSettings::SHORT_PWRBTN::BLOCK_FRONT) {
+    ignoreFrontButtons = !ignoreFrontButtons;
+  }
+
   // Enter reader menu activity.
-  if (mappedInput.wasReleased(MappedInputManager::Button::Confirm)) {
+  if (mappedInput.wasReleased(MappedInputManager::Button::Confirm) && !ignoreFrontButtons) {
     const int currentPage = section ? section->currentPage + 1 : 0;
     const int totalPages = section ? section->pageCount : 0;
     float bookProgress = 0.0f;
@@ -158,14 +163,15 @@ void EpubReaderActivity::loop() {
   }
 
   // Long press BACK (1s+) goes to file selection
-  if (mappedInput.isPressed(MappedInputManager::Button::Back) && mappedInput.getHeldTime() >= ReaderUtils::GO_HOME_MS) {
+  if (mappedInput.isPressed(MappedInputManager::Button::Back) && mappedInput.getHeldTime() >= ReaderUtils::GO_HOME_MS &&
+      !ignoreFrontButtons) {
     activityManager.goToFileBrowser(epub ? epub->getPath() : "");
     return;
   }
 
   // Short press BACK goes directly to home (or restores position if viewing footnote)
   if (mappedInput.wasReleased(MappedInputManager::Button::Back) &&
-      mappedInput.getHeldTime() < ReaderUtils::GO_HOME_MS) {
+      mappedInput.getHeldTime() < ReaderUtils::GO_HOME_MS && !ignoreFrontButtons) {
     if (footnoteDepth > 0) {
       restoreSavedPosition();
       return;
--- src/activities/reader/EpubReaderActivity.h
+++ src/activities/reader/EpubReaderActivity.h
@@ -27,7 +27,7 @@ class EpubReaderActivity final : public Activity {
   bool pendingScreenshot = false;
   bool skipNextButtonCheck = false;  // Skip button processing for one frame after subactivity exit
   bool automaticPageTurnActive = false;
-
+  bool ignoreFrontButtons = false;
   // Footnote support
   std::vector<FootnoteEntry> currentPageFootnotes;
   struct SavedPosition {
