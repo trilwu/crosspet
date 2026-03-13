ec3193fdb622869360d47610e082edffd33b94b3
quannt-pengu <quannt@vilead.vn>
2026-03-13 11:36:28 +0700

    feat: add sleep screen cache

--- src/activities/tools/ToolsActivity.cpp
+++ src/activities/tools/ToolsActivity.cpp
@@ -14,6 +14,7 @@
 #include "PresenterActivity.h"
 #include "WeatherActivity.h"
 #include "ReadingStatsActivity.h"
+#include "SleepCacheClearActivity.h"
 #include "SleepImagePickerActivity.h"
 #include "components/UITheme.h"
 #include "fontIds.h"
@@ -59,20 +60,23 @@ void ToolsActivity::loop() {
       case 6:
         activityManager.pushActivity(std::make_unique<SleepImagePickerActivity>(renderer, mappedInput));
         break;
-      // -- Games --
       case 7:
+        activityManager.pushActivity(std::make_unique<SleepCacheClearActivity>(renderer, mappedInput));
+        break;
+      // -- Games --
+      case 8:
         activityManager.pushActivity(std::make_unique<ChessActivity>(renderer, mappedInput));
         break;
-      case 8:
+      case 9:
         activityManager.pushActivity(std::make_unique<CaroActivity>(renderer, mappedInput));
         break;
-      case 9:
+      case 10:
         activityManager.pushActivity(std::make_unique<SudokuActivity>(renderer, mappedInput));
         break;
-      case 10:
+      case 11:
         activityManager.pushActivity(std::make_unique<MinesweeperActivity>(renderer, mappedInput));
         break;
-      case 11:
+      case 12:
         activityManager.pushActivity(std::make_unique<TwentyFortyEightActivity>(renderer, mappedInput));
         break;
     }
@@ -95,6 +99,7 @@ void ToolsActivity::render(RenderLock&&) {
   const char* menuLabels[] = {
       tr(STR_CLOCK), tr(STR_WEATHER), tr(STR_POMODORO), tr(STR_VIRTUAL_PET),
       tr(STR_PRESENTER), tr(STR_READING_STATS_APP), tr(STR_SLEEP_IMAGE_PICKER),
+      tr(STR_RELOAD_SLEEP_IMAGE),
       tr(STR_CHESS), tr(STR_CARO), tr(STR_SUDOKU), tr(STR_MINESWEEPER), tr(STR_2048)};
 
   const int menuTop = metrics.topPadding + metrics.headerHeight + metrics.verticalSpacing;
